ae6d81bc45edda40fa86860fdfd34fec57634f19
redsnowc <nuanyang.44@gmail.com>
2019-11-11 14:26:16 +0800

    init

---
+++ app.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Back, Style
+
+print(Fore.LIGHTYELLOW_EX + "Hello World!")
+print(Back.LIGHTGREEN_EX + "Hello Wrold!")
+
---
+++ settings/settings.py
@@ -0,0 +1,9 @@
+# 数据库连接配置文件
+mysql_config = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "1234",
+    "database": "vega",
+    "pool_size": 10,
+}
